b3367eaf5ad1993270d07ed6398dbfc1098171bd
Magevo <jackson.scown@icloud.com>
2022-11-05 19:05:30 +1000

    Seventh Commit

--- Json/MapManager.js
+++ Json/MapManager.js
@@ -1,4 +1,4 @@
-let Floor,Grass,Trees;
+let Floor,Floor2,Grass,Trees,Floral,Fill,Bush,Wave;
 
 let XFloor1,XFloor2,YFloor;
 let ScrollSpeedFloor;
@@ -9,14 +9,27 @@ let ScrollSpeedGrass;
 let XTrees1,XTrees2,YTrees;
 let ScrollSpeedTrees;
 
+let XFloral1,XFloral2,YFloral;
+let ScrollSpeedFloral;
 
+let XFill1,XFill2,YFill;
+let ScrollSpeedFill;
 
+let XBush1,XBush2,YBush;
+let ScrollSpeedBush;
 
-let FloorSpeed = 1;
-let GrassSpeed = 0.4;
-let TreesSpeed = 0.1;
+let XWave1,XWave2,YWave;
+let ScrollSpeedWave;
 
+let FloorSpeed = 20;
+let GrassSpeed = 5;
+let TreesSpeed = 2;
+let FloralSpeed = 2;
+let FillSpeed = 2;
+let BushSpeed = 2;
+let WaveSpeed = 2;
 
+let FloorBig,GrassBig,TreesBig;
 
 
 
@@ -31,6 +44,15 @@ preload(){
     Floor = loadImage("/Images/Maps/Purple/Floor.png");
     Grass = loadImage("/Images/Maps/Purple/Grass.png");
     Trees = loadImage("Images/Maps/Purple/Trees.png");
+    Floor2 = loadImage("/Images/Maps/Blue/Floor.png");
+    Floral = loadImage("Images/Maps/Purple/Floral.png");
+    Fill = loadImage("Images/Maps/Purple/Fill.png");
+    Bush = loadImage("Images/Maps/Purple/Bush.png");
+    Wave = loadImage("Images/Maps/Purple/Wave.png");
+
+    // Floor = FloorBig.resize(W,H);
+    // Grass = GrassBig.resize(W,H);
+    // Trees = TreesBig.resize(W,H);
 
 }
 
@@ -50,12 +72,35 @@ setup(){
     XTrees2 = W
     ScrollSpeedTrees = TreesSpeed;
 
+    YFloral = 0
+    XFloral1 = 0
+    XFloral2 = W
+    ScrollSpeedFloral = FloralSpeed;
+
+    YFill = 0
+    XFill1 = 0
+    XFill2 = W
+    ScrollSpeedFill = FillSpeed;
     
+    YBush = 0
+    XBush1 = 0
+    XBush2 = 0
+    ScrollSpeedBush = BushSpeed;
+
+    YWave = 0
+    XWave1 = 0
+    XWave2 = W
+    ScrollSpeedWave = WaveSpeed;
 
 }
 
 draw(){
 
+
+this.drawFill();
+this.drawWave();
+this.drawBushes();
+this.drawFloral();
 this.drawTrees();
 this.drawGrass();
 this.drawFloor();
@@ -64,8 +109,6 @@ this.drawFloor();
 
 }
 
-
-
 drawFloor(){
 
     image(Floor,XFloor1,YFloor,W,H);
@@ -73,16 +116,16 @@ drawFloor(){
 
 
     if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
-    XFloor1 += ScrollSpeedFloor
-    XFloor2 += ScrollSpeedFloor
+    XFloor1 -= ScrollSpeedFloor
+    XFloor2 -= ScrollSpeedFloor
     }
 
-    if (XFloor1 > W){
-        XFloor1 = -(W);
+    if (XFloor1 < -W){
+        XFloor1 = (W) - FloorSpeed / 2;
     }
 
-    if (XFloor2 > W){
-        XFloor2 = -(W) + FloorSpeed;
+    if (XFloor2 < -W){
+        XFloor2 = (W) ;
     }
 
 }
@@ -96,41 +139,135 @@ drawGrass(){
 
     if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
 
-    XGrass1 += ScrollSpeedGrass
-    XGrass2 += ScrollSpeedGrass
+    XGrass1 -= ScrollSpeedGrass
+    XGrass2 -= ScrollSpeedGrass
     }
 
-    if (XGrass1 > W){
-        XGrass1 = -(W);
+    if (XGrass1 < -W){
+        XGrass1 = (W) - GrassSpeed/2;
     }
 
-    if (XGrass2 > W){
-        XGrass2 = -(W) + GrassSpeed;
+    if (XGrass2 < -W){
+        XGrass2 = (W);
     }
 }
 
 drawTrees(){
     image(Trees,XTrees1,YTrees,W,H);
-    image(Trees,XTrees1,YTrees,W,H);
+    image(Trees,XTrees2,YTrees,W,H);
     
-
+    
 
     if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
 
-    XTrees1 += ScrollSpeedTrees
-    XTrees2 += ScrollSpeedTrees
+    XTrees1 -= ScrollSpeedTrees
+    XTrees2 -= ScrollSpeedTrees
     }
 
-    if (XTrees1 > W){
-        XTrees1 = -(W);
+    if (XTrees1 < -W){
+        XTrees1 = (W) - TreesSpeed/2;
     }
 
-    if (XTrees1 > W){
-        XTrees2 = -(W) + TreesSpeed;
+    if (XTrees2 < -W){
+        XTrees2 = (W);
     }
 
 }
 
+drawFloral(){
 
+    image(Floral,XFloral1,YFloral,W,H);
+    image(Floral,XFloral2,YFloral,W,H);
+    
+    
+
+    if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
+
+        XFloral1 -= ScrollSpeedTrees
+        XFloral2 -= ScrollSpeedTrees
+    }
+
+    if (XFloral1 < -W){
+        XFloral1 = (W) - FloralSpeed/2;
+    }
+
+    if (XFloral2 < -W){
+        XFloral2 = (W);
+    }
+
+
+}
+
+drawFill(){
+
+    image(Fill,XFill1,YFill,W,H);
+    image(Fill,XFill2,YFill,W,H);
+    
+    
+
+    if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
+
+        XFill1 -= ScrollSpeedTrees
+        XFill2 -= ScrollSpeedTrees
+    }
+
+    if (XFill1 < -W){
+        XFill1 = (W) - FloralSpeed/2;
+    }
+
+    if (XFill2 < -W){
+        XFill2 = (W);
+    }
+
+
+}
+
+drawBushes(){
+
+    image(Bush,XBush1,YBush,W,H);
+    image(Bush,XBush2,YBush,W,H);
+    
+    
+
+    if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
+
+        XBush1 -= ScrollSpeedTrees
+        XBush2 -= ScrollSpeedTrees
+    }
+
+    if (XBush1 < -W){
+        XBush1 = (W) - FloralSpeed/2;
+    }
+
+    if (XBush2 < -W){
+        XBush2 = (W);
+    }
+
+
+}
+
+drawWave(){
+
+    image(Wave,XWave1,YWave,W,H);
+    image(Wave,XWave2,YWave,W,H);
+    
+    
+
+    if(keyIsDown(KEYS.Right) & RecoveryCounter < 1){
+
+        XWave1 -= ScrollSpeedWave
+        XWave2 -= ScrollSpeedWave
+    }
+
+    if (XWave1 < -W){
+        XWave1 = (W) - WaveSpeed/2;
+    }
+
+    if (XWave2 < -W){
+        XWave2 = (W);
+    }
+
+
+}
 
 }
